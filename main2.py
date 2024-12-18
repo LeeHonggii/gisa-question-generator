cf6873488a5dc27fea557f45c5dde359068bdc63
이홍기 <dughdrl7@gmail.com>
2024-12-18 23:57:51 +0900

    test

--- main2.py
+++ main2.py
@@ -111,4 +111,4 @@ async def generate_question(request: QuestionRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8800)
